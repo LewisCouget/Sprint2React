c37b62f8469b1494e2a5469d9c1c83238532f13b
LewisCouget <lewis_2051@hotmail.com>
2024-10-27 17:02:30 -0300

    Sprint2

---
+++ sprint2/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Header from "./components/header";
+import Main from "./components/Main";
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Section />
+    </>
+  );
+}
+export default App;
---
+++ sprint2/src/components/Section.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const section = () => {
+  return (
+    <section>
+      <h3>Why Us</h3>
+      <p>
+        Tutorials by industry experts Peer & expert code review Coding exercises
+        Access to our GitHub repos Community forum Flashcard decks New videos
+        every week
+      </p>
+    </section>
+  );
+};
+
+export default section;
